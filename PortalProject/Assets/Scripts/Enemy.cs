1b1f599eca452157331607b94556a7c5b406f5b6
sunriseL <540729251@qq.com>
2019-05-21 21:41:35 +0800

    初步完成EnemyFire与Enemy

---
+++ PortalProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject fire;
+
+    public GameObject fp;
+    public float interval;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot",0,interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        Rigidbody clone = Instantiate(fire,fp.transform.position,fp.transform.rotation).GetComponent<Rigidbody>();
+        clone.velocity = transform.TransformDirection(fp.transform.forward*speed);
+    }
+}
---
+++ PortalProject/Assets/Scripts/EnemyFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Wall")) {
+            GameObject.Destroy(this.gameObject);
+        }else if(other.gameObject.CompareTag("Player")) {
+            ResetStage();
+        }else if(other.gameObject.CompareTag("Enemy")) {
+            KillEnemy(other.gameObject);
+        }
+    }
+
+    private void KillEnemy(GameObject enemy) {
+        //  TODO
+        GameObject.Destroy(enemy);
+        return;
+    }
+
+    private void ResetStage() {
+        //  TODO
+        return;
+    }
+
+}
