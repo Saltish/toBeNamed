6d4ff250cad49490b65954566d9b5dd8b70e44f0
Saltish <c785180068@outlook.com>
2019-05-22 03:16:59 +0800

    basic move and teleport

---
+++ PortalProject/Assets/Saltish/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("---控制移动的速度---")]
+    public float speedMove = 5.0f;
+
+    private CharacterController characterController;
+
+    private Camera mainCamera;  //主摄像机
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        offset = mainCamera.transform.position - this.transform.position;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) { speedMove += 10.0f; }
+        float deltaX = Input.GetAxis("Horizontal") * speedMove;
+        float deltaZ = Input.GetAxis("Vertical") * speedMove;
+        Vector3 toMove = new Vector3(deltaX, 0, deltaZ);
+        toMove = Vector3.ClampMagnitude(toMove, speedMove);
+
+        //碰撞检测
+        toMove = transform.TransformDirection(toMove);
+        characterController.Move(toMove * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        mainCamera.transform.position = this.transform.position + offset;
+    }
+}
---
+++ PortalProject/Assets/Saltish/Scripts/Teleportable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportable : MonoBehaviour
+{
+    private Vector3 pos;
+    private bool moveFlag = false;
+    private int nowId = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (nowId == -1 && other.gameObject.tag == "Portal")
+        {
+            Debug.Log(1);
+            PortalObject nowPortal = other.gameObject.GetComponent<PortalObject>();
+            nowId = nowPortal.getId();
+            PortalObject anotherPortal = PortalController.getAnotherPortal(nowId);
+            Vector3 newPos = anotherPortal.transform.position;
+            pos = new Vector3(newPos.x, transform.position.y, newPos.z);
+            moveFlag = true;
+            //TODO 带角度
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Portal" && other.gameObject.GetComponent<PortalObject>().getId() != nowId)
+        {
+            nowId = -1;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (moveFlag)
+        {
+            transform.position = pos;
+            moveFlag = false;
+        }
+    }
+}
