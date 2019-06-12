2dc3bae10dbc06cf70b5624c0674ed047588b968
Decappoolla <823967701@qq.com>
2019-06-12 13:46:56 +0800

    操作提示气泡

---
+++ PortalProject/Assets/Scripts/HeadTopMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadTopMessage : MonoBehaviour
+{
+    //此脚本用于使某一UI图片跟随场景内某一物体
+    //此脚本应该挂在画布下的含Image组件的对象上
+
+    public Transform targetTrans;//本UI跟随哪个物体
+    public Vector3 UIoffset;//本UI相对于该物体的偏置
+    private Image _image;
+    public Sprite[] localSprite;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        transform.position = Camera.main.WorldToScreenPoint(targetTrans.position) + UIoffset;
+    }
+
+    public void SetImage(bool on, int num=0)
+    {
+        if (!on)
+        {
+            _image.enabled = false;
+            return;
+        }
+
+        _image.enabled = true;
+        _image.sprite = localSprite[num];
+    }
+    
+}
--- PortalProject/Assets/Scripts/Touch.cs
+++ PortalProject/Assets/Scripts/Touch.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Touch : MonoBehaviour
 {
     //带有这个脚本的对象 应具有trigger盒，属于touch层
-    public GameObject player;
     public GameObject facedObj;
 
+    public HeadTopMessage headTopMessage;
     // Update is called once per frame
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Portal"))
+        {
             facedObj = other.transform.parent.gameObject;
+            headTopMessage.SetImage(true, 0);
+        }
+
         else if (other.CompareTag("Treasure"))
+        {
             facedObj = other.gameObject;
+            headTopMessage.SetImage(true, 1);
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -23,6 +31,7 @@ public class Touch : MonoBehaviour
         if (other.CompareTag("Portal") || other.CompareTag("Treasure"))
         {
             facedObj = null;
+            headTopMessage.SetImage(false);
         }
     }
 
